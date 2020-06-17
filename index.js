368cda2a1b05af3e4e01596bc47bcc1bbf831f13
Kevin-wachel <kevwachel@gmail.com>
2020-06-17 14:31:49 +0200

    Du javascript vers le Html, ok

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 //Appel de l'Api
 const url = "http://localhost:3000/api/teddies";
 
-//Récupération des données en JSON
+//Fonction
 let request = new XMLHttpRequest();
 request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        let response = JSON.parse(this.responseText);
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) { //Vérification si c'est bon
+        let response = JSON.parse(this.responseText);                   //Récupération des données en JSON.parse
         console.log(response);
-        console.log(response[4]);
+        response.forEach(function (response) {                          //Du javascript vers le HTML, en boucle
+            let elt = document.getElementById('produit');               //Sélection de l'id produit
+            elt.innerHTML += '<li> Article </li>';                      //Création du li
+            elt.innerHTML += '<p>' + response.name + '</p>';            //Création du nom de l'article
+            elt.innerHTML += '<img src="' + response.imageUrl + '"/>';  //Création de l'image
+            elt.innerHTML += '<p>' + response.description + '<p>';      //Création de la description
+            elt.innerHTML += '<p>' + response.price + '</p>';           //Création du prix
+        });
     };
 };
 request.open("GET", url);
