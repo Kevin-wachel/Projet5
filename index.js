928e7ac292f388f53b3b176285304d7e9134378b
Kevin-wachel <kevwachel@gmail.com>
2020-06-24 10:50:07 +0200

    C'est bon ça marche, enfin!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-//Appel de l'Api
+// Appel de l'Api
 const url = "http://localhost:3000/api/teddies";
-let ul = document.querySelector('ul');
+// Séléction de la balise ul
+const ul = document.querySelector('ul');
 
 //Fonction
 let request = new XMLHttpRequest();
@@ -10,19 +11,36 @@ request.onreadystatechange = function() {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) { //Vérification si c'est bon
             let response = JSON.parse(this.responseText);                   //Récupération des données en JSON.parse
             console.log(response);
-            response.forEach(function (response) {                          //Du javascript vers le HTML, en boucle
-                let elt = document.getElementById('produit');               //Sélection de l'id produit
-                elt.innerHTML += '<li>  </li>';                             //Création du li
-                elt.innerHTML += '<p>' + response.name + '</p>';            //Création du nom de l'article
-                elt.innerHTML += '<img src="' + response.imageUrl + '"/>';  //Création de l'image
-                elt.innerHTML += '<p>' + response.description + '<p>';      //Création de la description
-                elt.innerHTML += '<p>' + response.price/100 + ' €</p>';     //Création du prix
-                elt.innerHTML += '<a href="produit.html">Page produit</a>'; //Lien vers la page produit
-            });
+
+            function maFonction() {                                         // Fonction pour intégrer les élèments dans le Html
+                for(let i = 0; i < response.length; i++) {                  // La boucle i
+                    const myLi = document.createElement('li');              // Création des élèments
+                    const myH3 = document.createElement('h3');
+                    const myImg = document.createElement('img');
+                    const myDescription = document.createElement('p');
+                    const myPrice = document.createElement('p');
+                    const myLien = document.createElement('a');
+                    myH3.textContent = response[i].name;
+                    myImg.setAttribute("src", response[i].imageUrl);
+                    myDescription.textContent = response[i].description;
+                    myPrice.textContent = response[i].price/100 + " €";
+                    myLien.textContent = "Lien vers le produit";
+                    myLien.setAttribute("href", "produit.html");
+
+                    myLi.appendChild(myH3);                                 // Intégration des élèments dans la balise li
+                    myLi.appendChild(myImg);
+                    myLi.appendChild(myDescription);
+                    myLi.appendChild(myPrice);
+                    myLi.appendChild(myLien);
+                    
+                    ul.appendChild(myLi);                                   // Intégration des élèments dans la balise ul
+                };
+            };
+            maFonction();                                                   // Appel de la fonction
+            
         };
     };
 };
 request.open("GET", url);
 request.send();
-
 
