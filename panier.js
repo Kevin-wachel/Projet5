043b6416e57925d46def97735696ca7d73547e9f
Kevin-wachel <kevwachel@gmail.com>
2020-07-19 21:51:05 +0200

    Un peu de Css + ajout du bouton pour supprimer tout les articles.

--- panier.js
+++ panier.js
@@ -18,20 +18,45 @@ function commandeOursEnPeluche() {
         const myTd3 = document.createElement('td');
         const myTd4 = document.createElement('td');
         const myTd5 = document.createElement('td');
+        const myDeleteButton = document.createElement('button');
+        const myDeleteAllButton = document.querySelector('.btn_alldelete');
 
         myTd1.textContent = commande[i].name;
         myTd2.textContent = commande[i].colors;
         myTd3.textContent = commande[i].price/100 + " €";
         myTd4.textContent = commande[i].qte;
         myTd5.textContent = (commande[i].price/100) * commande[i].qte + " €";
+        myDeleteButton.textContent = "Supprimer";
 
         myTr.appendChild(myTd1);
         myTr.appendChild(myTd2);
         myTr.appendChild(myTd3);
         myTr.appendChild(myTd4);
         myTr.appendChild(myTd5);
+        myTr.appendChild(myDeleteButton);
 
         tbody.appendChild(myTr);
+
+        // Supprime un article
+        /*myDeleteButton.addEventListener('click', function (event) {
+            function deleteItem() {
+ 
+                window.location.reload()
+            };
+            deleteItem();
+        });       */
+        myDeleteButton.setAttribute("onclick", "deleteItem()")
+
+        // Supprime tout les articles
+        myDeleteAllButton.addEventListener('click', function (event) {
+            function deleteAllItem() {
+                localStorage.removeItem("obj"); 
+                window.location.reload()
+            };
+            deleteAllItem();
+        });       
+        myDeleteAllButton.setAttribute("onclick", "deleteAllItem()")
+
 
     };
 };
