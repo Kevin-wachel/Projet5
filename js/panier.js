8d6253d05daf3e520d06313b6767c69da71b4d4e
Kevin-wachel <kevwachel@gmail.com>
2020-09-11 14:18:47 +0200

    Ok

--- js/panier.js
+++ js/panier.js
@@ -74,49 +74,94 @@ commandeOursEnPeluche();
 
 const myButtonSubmit = document.querySelector('.btn');
 
+function checkInput() {
+    // Regex
+    let checkString = /^[A-Z]{1}[a-zéèêàçî]/;
+    let checkMail = /.+@.+\..+/;
+    let checkAdresse = /^[^@&"()!_$*€£`%+=\/;?#]+$/;
+  
+    // Inputs de l'utilisateur
+    let formPrenom = document.querySelector('#firstName').value;
+    let formNom = document.querySelector('#lastName').value;
+    let formAdresse = document.querySelector('#address').value;
+    let formVille = document.querySelector('#city').value;
+    let formMail = document.querySelector('#email').value;
+  
+    // Vérifier les inputs de l'utilisateur
+    if (checkString.test(formPrenom) == false) {
+        alert("Votre prénom doit commencer par une majuscule suivis de minuscules");
+        return false;
+    } else if (checkString.test(formNom) == false) {
+        alert("Votre nom doit commencer par une majuscule suivis de minuscules");
+        return false;
+    } else if (checkAdresse.test(formAdresse) == false) {
+        alert(
+            `Votre adresse contient un ou plusieurs des caractères interdits suivants : ` +
+            '[^@&"()!_$*€£`%+=/;?#]' +
+            " ou n'est pas renseignée."
+        );
+        return false;
+    } else if (checkString.test(formVille) == false) {
+        alert(
+            "Le nom de votre ville doit commencer par une majuscule suivis de minuscules"
+        );
+        return false;
+    } else if (checkMail.test(formMail) == false) {
+        alert("Votre email doit être au format xxx@yyy.zzz");
+        return false;
+    } else {
+        return true;
+    }
+};
+
+
 myButtonSubmit.addEventListener('click', function (event) {
     event.preventDefault()
-    // Récupération des données saisie par l'utilisateur
-    let contact = {
-        firstName: document.querySelector('#firstName').value,
-        lastName: document.querySelector('#lastName').value,
-        address: document.querySelector('#address').value,
-        city: document.querySelector('#city').value,
-        email: document.querySelector('#email').value
-    };
+    if (checkInput() == true) {
+        // Récupération des données saisie par l'utilisateur
+        let contact = {
+            firstName: document.querySelector('#firstName').value,
+            lastName: document.querySelector('#lastName').value,
+            address: document.querySelector('#address').value,
+            city: document.querySelector('#city').value,
+            email: document.querySelector('#email').value
+        };
 
-    // Récupération de la commande
-    let products = [];
-    for (let i = 0; i < commande.length; i++) {
-        products.push(commande[i].id)
-    };
+        // Récupération de la commande
+        let products = [];
+        for (let i = 0; i < commande.length; i++) {
+            products.push(commande[i].id)
+        };
 
-    // Ajout des données de contact et produit dans data
-    let dataPanier = { contact, products };
+        // Ajout des données de contact et produit dans data
+        let dataPanier = { contact, products };
 
-    // Création de la methode 
-    const envoiePost = fetch("http://localhost:3000/api/teddies/order", {
-        method: "POST",
-        body: JSON.stringify(dataPanier),    
-        headers: {
-            "Content-Type": "application/json"
-        }       
-    });
+        // Création de la methode 
+        const envoiePost = fetch("http://localhost:3000/api/teddies/order", {
+            method: "POST",
+            body: JSON.stringify(dataPanier),    
+            headers: {
+                "Content-Type": "application/json"
+            }       
+        });
 
-    // Envoie des données au serveur
-    envoiePost.then ( async response => {
-        try {
-            console.log(response);
-            const body = await response.json();
-            console.log(body);
-            const myReponse = JSON.stringify(body);
-            localStorage.setItem("commande", myReponse);
-            window.location.href = "confirm.html";
-        }catch(e) {
-            console.log(e);
-        }
-    });
-    
-    
+        // Envoie des données au serveur
+        envoiePost.then ( async response => {
+            try {
+                console.log(response);
+                const body = await response.json();
+                console.log(body);
+                const myReponse = JSON.stringify(body);
+                localStorage.setItem("commande", myReponse);
+                window.location.href = "confirm.html";
+            }catch(e) {
+                console.log(e);
+            }
+        }); 
+    }; 
 });
+
+    
+
+
 
